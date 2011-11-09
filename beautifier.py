97ca33c59c2fe1b5bdcef55a48a4a6b419b9d27f
Kevin Saliou <kevin@b2deal.com>
2011-11-09 21:13:40 +0100

    initial commit : mix PhpBeautifier and JsFormat

---
+++ beautifier.py
@@ -0,0 +1,32 @@
+import commands
+import jsbeautifier
+import sublime, sublime_plugin
+
+class BeautifierCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        FILE = self.view.file_name()
+        if FILE[-4:] == '.php':
+            print __name__ + ' Calling php_beautifier'
+            PHP_OPTIONS = "-s4 --filters \'ArrayNested() DocBlock() NewLines(before=switch:while:T_COMMENT:for:foreach:T_CLASS:return:break,after=T_COMMENT:protected:private)\' "
+            selection = self.view.sel()[0]
+            replaceRegion = selection if len(selection) > 0 else sublime.Region(0, self.view.size())
+            f = open('/tmp/asdf', 'w')
+            f.write(self.view.substr(replaceRegion))
+            f.close()
+            cmd = "php_beautifier " + PHP_OPTIONS + "--input /tmp/asdf --output -"
+            print __name__ + ' ' + cmd
+            res = commands.getoutput(cmd)
+            resu = unicode( res, "utf-8" )
+            self.view.replace(edit, replaceRegion, resu)
+        if FILE[-3:] == '.js':
+            print __name__ + 'Calling jsbeautifier'
+            opts = jsbeautifier.default_options();
+            opts.indent_char = "\t"
+            opts.indent_size = 1
+            opts.max_preserve_newlines = 3
+            selection = self.view.sel()[0]
+            replaceRegion = selection if len(selection) > 0 else sublime.Region(0, self.view.size())
+            res = jsbeautifier.beautify(self.view.substr(replaceRegion), opts)
+            prePos = self.view.sel()[0]
+            self.view.replace(edit, replaceRegion, res)
+            self.view.show_at_center(prePos.begin())
---
+++ readme.md
@@ -0,0 +1,7 @@
+Summary
+=======
+
+This is mix between the PhpBeautifier (https://github.com/SublimeText/PhpBeautifier) and JsFormat (https://github.com/jdc0589/JsFormat) plugins.
+Both were interesting to me, but their key bindings being the same, they were conflicting
+
+To install it i'd recommend using the great Package Control plugin (https://github.com/wbond/sublime_package_control), and add this repository to your personal list of repositories. Maybe one day it will make it to the "official" package control channel! :)
